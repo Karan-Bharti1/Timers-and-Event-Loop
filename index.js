0003f66da639b296cfef6b1261fd63ed1ce1fb0a
Karan-Bharti1 <karan.codes1@gmail.com>
2025-03-21 15:17:17 +0530

    Clear Timers  to stop set Interval after a time using setTimeout

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function sayHello(){
 // setTimeout(sayHello,5000)
 
 //setInterval runs a piece of code at a specified interval repeatedly
-setInterval(sayHello,2000)
+
+//Clear Timers to stop or clear this setInterval
+let timerId=setInterval(sayHello,2000)
+setTimeout(()=>{
+clearInterval(timerId) //clear Interval web Api
+console.log("Time Stopped")
+},8000)
