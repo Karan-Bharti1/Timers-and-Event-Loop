a2373d07dd7196fd8a8509801d45227fec4a2732
Karan-Bharti1 <karan.codes1@gmail.com>
2025-03-21 15:20:26 +0530

    clearTimeout  basics

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 function sayHello(){
     console.log("Hello, world")
 }
-// setTimeout(sayHello,5000)
-
+let timeOutId=setTimeout(sayHello,5000)
+clearTimeout(timeOutId)
 //setInterval runs a piece of code at a specified interval repeatedly
 
 //Clear Timers to stop or clear this setInterval
-let timerId=setInterval(sayHello,2000)
-setTimeout(()=>{
-clearInterval(timerId) //clear Interval web Api
-console.log("Time Stopped")
-},8000)
+// let timerId=setInterval(sayHello,2000)
+// setTimeout(()=>{
+// clearInterval(timerId) //clear Interval web Api
+// console.log("Time Stopped")
+// },8000)
