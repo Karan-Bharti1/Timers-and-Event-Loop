8d7154ba7a467c8222d74565ffe528b2969d8564
Karan-Bharti1 <karan.codes1@gmail.com>
2025-03-21 15:13:41 +0530

    basics of setIntervals

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 function sayHello(){
     console.log("Hello, world")
 }
-setTimeout(sayHello,5000)
+// setTimeout(sayHello,5000)
+
+//setInterval runs a piece of code at a specified interval repeatedly
+setInterval(sayHello,2000)
